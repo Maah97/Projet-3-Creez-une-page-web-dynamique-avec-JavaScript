ebd3244531633a378a1fe883ecfc29d80f97a822
Maah97 <mahmoudouaboul@gmail.com>
2024-05-26 13:48:15 +0100

    Recupération des données depuis le backend

---
+++ FrontEnd/assets/projet.js
@@ -0,0 +1,13 @@
+const reponse = await fetch('http://localhost:5678/api/works');
+const travaux = await reponse.json();
+let galerie = document.querySelector(".gallery");
+for (let i = 0; i < travaux.length; i++) {
+    galerie.innerHTML += `
+        <figure>
+            <img src="${travaux[i].imageUrl}" alt="${travaux[i].title}">
+            <figcaption>${travaux[i].title}</figcaption>
+        </figure>
+    `
+}
+console.log(galerie);
+console.log(travaux);
